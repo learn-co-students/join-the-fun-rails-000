9ed0efd1e6348c407983db015b921699900e8864
gjc2118 <gjc2118@gmail.com>
2015-07-13 23:32:20 -0700

    done

---
+++ db/migrate/20140212206042_create_rides.rb
@@ -0,0 +1,10 @@
+class CreateRides < ActiveRecord::Migration
+  def change
+    create_table :rides do |t|  
+      t.timestamps null: false
+      t.belongs_to :taxi, index: true
+      t.belongs_to :passenger, index: true
+    end
+  end
+end
+  
